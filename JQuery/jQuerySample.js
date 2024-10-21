d679e646bc5f1c8833f95f9b77fd5e9010f9028c
TMatis <Tmatis@purdue.edu>
2024-10-21 11:23:06 -0400

    inn comit

---
+++ JQuery/jQuerySample.js
@@ -0,0 +1,15 @@
+const setUp = () => {
+
+    $("#animalProfile").text(
+  
+      "Buhmie the bulldog is the most playful dog in the pack."
+  
+    );
+  
+    $("h3").text("Buhmie The Bulldog, 2 Years Old");
+  
+  };
+  
+  
+  
+  $(document).ready(setUp);
---
+++ JQuery/sample.js
@@ -0,0 +1,11 @@
+let petReference = document.querySelector("#animalProfile");
+
+petReference.textContent =
+
+  "Buhmie the bulldog is the most playful dog in the pack.";
+
+let headingReference = document.querySelector("h3");
+
+console.log(headingReference);
+
+headingReference.textContent = "Buhmie The Bulldog, 2 Years Old";
